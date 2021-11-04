757f60f16e2048832aebb369e0287652db9f745f
Aidyn <adilkhan.a@rahmetapp.kz>
2021-11-04 13:16:59 +0600

    third task - drag and drop

---
+++ src/test/java/github/DragAndDrop.java
@@ -0,0 +1,32 @@
+package github;
+
+import com.codeborne.selenide.*;
+import org.junit.jupiter.api.*;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragAndDrop {
+
+    @Test
+    void dragDrop() {
+        Configuration.startMaximized = true;
+
+        //Открытие браузера
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+//        actions().clickAndHold($("#column-a"))
+//                .moveToElement($("#column-b"));
+//                .perform();
+
+        //Перенос квадрата А в квадрат Б
+        $("#column-a").dragAndDropTo($("#column-b"));
+
+        //Поверка что квадраты поменялись
+        $("#column-a header").shouldHave(text("B"));
+
+    }
+}
